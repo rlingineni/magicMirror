a101b666d8b7059bebef5abf09882b548de75f40
rlingineni <Raviteja@Ravitejas-MacBook-Pro.local>
2016-02-02 21:20:58 -0600

    added wind Data

--- js/weather.js
+++ js/weather.js
@@ -8,9 +8,12 @@ function getWeather()
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>' ;
+      html += '<p>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</p>';
+
+        
       $("#weather").html(html);
+   
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
